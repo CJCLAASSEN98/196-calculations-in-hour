8838df91e3d4b5eb0eb557fd112af93ff2f3f7a2
CJ <claassen.cjs@gmail.com>
2021-10-20 14:50:58 -0700

    first commit

---
+++ src/PalindromeProblem.ConsoleApp/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+
+namespace PalindromeProblem
+{
+    class Program
+    {   
+        static void Main(string[] args)
+        
+        {
+            
+            string num = "196";
+            bool pal = Palindrome.IsPalindrome(num);
+            string tempv = "";
+            string revN = "";
+            long i = 0;
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+
+            StreamWriter sw = new StreamWriter(@"Iterations.txt");
+            sw.WriteLine("The numer is {0}", num);
+            sw.WriteLine("-----------------------");
+            sw.WriteLine("The Application started at {0:HH:mm:ss.fff}", DateTime.Now);
+
+
+
+            while(pal == false && watch.ElapsedMilliseconds <= 3600000)
+            {
+                if (pal)
+                {
+                    System.Console.WriteLine(num);
+                    break;
+                }
+                else
+                {
+
+
+                    revN = Utils.RevNum(num);
+                    tempv = Utils.AddString(num, revN);
+                    System.Console.WriteLine(tempv);
+                    num = tempv;
+                    pal = Palindrome.IsPalindrome(num);
+                    i++;
+                    sw.WriteLine("Iteration {0} || Value {1}", i, num);
+                    sw.WriteLine("=====================================================");
+                    sw.WriteLine("Iteration {0} TimeStamp {1:HH:mm:ss.fff}",i, DateTime.Now);
+                    sw.WriteLine("_____________________________________________________");
+
+
+                    
+                }
+
+                if (pal){sw.Close();}
+            }
+
+
+
+
+
+
+
+          
+
+
+
+
+        }
+
+
+
+
+        
+    } 
+}
---
+++ src/PalindromeProblem.Utils/Utils.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace PalindromeProblem
+{
+    public class Utils
+    {
+        public static string RevNum(string str)
+        {
+
+            string  revString = "";
+            for(int i=str.Length-1; i>=0; i--)
+            {
+                revString = revString + str[i];
+            }
+            return revString;
+        }
+
+        public static string AddString(string num1, string num2)
+        {
+            if(num1.Length > num2.Length)
+            {
+                string t = num1;
+                num1 = num2;
+                num2 = t;
+            }
+
+            string str = "";
+            int n1 = num1.Length, n2 = num2.Length;
+            int diff = n2 - n1;
+            int carry = 0;
+
+            for(int i = n1 -1; i >= 0; i--)
+            {
+                int sum = ((int)(num1[i] - '0') + (int)(num2[i + diff]-'0') + carry);
+                str += (char)(sum % 10 + '0');
+                carry = sum / 10;
+            }
+
+            for(int i = n2 - n1 - 1; i >= 0; i--)
+            {
+                int sum = ((int)(num2[i] - '0') + carry);
+                str += (char)(sum % 10 + '0');
+                carry = sum / 10 ;
+            }
+
+            if (carry > 0) str += (char)(carry + '0');
+
+            char[] ch2 = str.ToCharArray();
+            Array.Reverse(ch2);
+            return new string(ch2);
+        }
+    }
+}
